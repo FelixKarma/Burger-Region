29cb89aae5505e373a86475cc4357652a6675eb8
ClemBlix <clementbailleux.pro@gmail.com>
2023-01-04 15:21:24 +0100

    jeux score

--- jeux/jeux.js
+++ jeux/jeux.js
@@ -1,5 +1,6 @@
 const dino = document.getElementById("dino");
 const cactus = document.getElementById("cactus");
+let currentScore = 0;
 
 function jump() {
   if (dino.classList != "jump") {
@@ -7,7 +8,7 @@ function jump() {
 
     setTimeout(function () {
       dino.classList.remove("jump");
-    }, 300);
+    }, 400);
   }
 }
 
@@ -23,10 +24,37 @@ let isAlive = setInterval(function () {
   // detect collision
   if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140) {
     // collision
-    alert("Game Over!");
+    // alert("Ton score: " + Math.round(currentScore));
+    if (window.confirm("Ton score: " + Math.round(currentScore)))
+{
+  window.location = 'http://127.0.0.1:5500/Burger-Region/jeux/jeux.html';
+}
+
   }
 }, 10);
 
 document.addEventListener("keydown", function (event) {
   jump();
 }); 
+
+// let currentScore = 0;
+
+function updateScore() {
+  // Sélectionne l'élément div avec l'id "game-score"
+  const scoreElement = document.getElementById("game-score");
+
+  // Met à jour le contenu HTML de l'élément avec le score actuel
+  scoreElement.innerHTML = "Score: " + currentScore;
+}
+const startTime = Date.now();
+
+function updateScore() {
+  const currentTime = Date.now();
+  const elapsedTime = (currentTime - startTime) / 1000;
+  currentScore += elapsedTime;
+
+  // Mise à jour de l'affichage du score
+  const scoreElement = document.getElementById("game-score");
+  scoreElement.innerHTML = "Score: " + Math.round(currentScore);
+}
+setInterval(updateScore, 1000);
